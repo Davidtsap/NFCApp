6526f2953433127f020536619236d945769cd298
David Tsap <daviddd05@gmail.com>
2018-04-24 00:37:56 +0300

    add user add chip

--- App/Logic/src/Data/Chip.java
+++ App/Logic/src/Data/Chip.java
@@ -4,4 +4,16 @@ public class Chip {
 
     String serialNumber;
     String action;
+    public void setSerialNumber(String serialNumber){
+        this.serialNumber =serialNumber;
+    }
+    public void setAction(String action){
+        this.action= action;
+    }
+    public String getSerialNumber(){
+        return this.serialNumber;
+    }
+    public String getAction(){
+        return this.action;
+    }
 }
